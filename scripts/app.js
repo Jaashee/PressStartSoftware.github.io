43ce88db0937f4f1c16a66ebe50d34c07bd822cc
Liamm602 <liam.barton@dcmail.ca>
2023-10-18 18:07:28 -0400

    Market button Functionality and buy page design

---
+++ scripts/app.js
@@ -0,0 +1,6 @@
+let marketButton =document.getElementById('market');
+let market = 'https://www.pricecharting.com/category/video-games';
+
+marketButton.addEventListener('click', () =>{
+window.open('https://www.pricecharting.com/category/video-games', '_blank')
+});
