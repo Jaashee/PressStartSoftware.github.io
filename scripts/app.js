c779e43a783946451efe40f9343a0aaf610ee001
Jaashee <jashakan2000@gmail.com>
2023-10-19 05:05:55 -0400

    Rough-in of NavBar and pag navigations

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,12 @@
-var express = require('express'),
-    path = require('path'),
-    bodyParser = require('body-parser'),
-    cons = require('consolidate'),
-    dust = require('dustjs-helpers'),
-    pg = require('pg'),
-    app = express();
+(function () {
 
-    //connection string to database
-var connect = "postgres://pressstart_admin:12345@localhost/PressStart_db"
+    let btn = document.querySelector('#btn');
+
+    let sidebar = document.querySelector('.sidebar');
+
+    btn.onclick = function () {
+        sidebar.classList.toggle("active");
+    };
 
 
-//getting element from button
-let marketButton =document.getElementById('market');
-//event listener so when button is clicked to redirets user to specified website
-marketButton.addEventListener('click', () =>{
-window.open('https://www.pricecharting.com/category/video-games', '_blank')
-});
+})();
