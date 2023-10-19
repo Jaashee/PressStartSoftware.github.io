8135c3d75c2dc303830c3677b03b4b4fb3edccc7
Liamm602 <liam.barton@dcmail.ca>
2023-10-19 12:29:52 -0400

    Added Database string

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,19 @@ let market = 'https://www.pricecharting.com/category/video-games';
 
 marketButton.addEventListener('click', () =>{
 window.open('https://www.pricecharting.com/category/video-games', '_blank')
+
+var express = require('express'),
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    cons = require('consolidate'),
+    dust = require('dustjs-helpers'),
+    pg = require('pg'),
+    app = express();
+
+    //connection string to database
+var connect = "postgres://pressstart_admin:12345@localhost/PressStart_db"
+
+
 });
 
 (function () {
