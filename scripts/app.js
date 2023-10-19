6fa84079391883851eda0fb1db38c5cc85abb0f3
Liamm602 <liam.barton@dcmail.ca>
2023-10-19 12:27:12 -0400

    fixed buy page

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,10 @@
+let marketButton =document.getElementById('market');
+let market = 'https://www.pricecharting.com/category/video-games';
+
+marketButton.addEventListener('click', () =>{
+window.open('https://www.pricecharting.com/category/video-games', '_blank')
+});
+
 (function () {
 
     let btn = document.querySelector('#btn');
