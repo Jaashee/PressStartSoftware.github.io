fadb36f0ec6e8970646d9e5f90461d3bdd121463
Liamm602 <liam.barton@dcmail.ca>
2023-10-18 22:02:01 -0400

    connection string to database some package.json updates

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,18 @@
-let marketButton =document.getElementById('market');
-let market = 'https://www.pricecharting.com/category/video-games';
+var express = require('express'),
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    cons = require('consolidate'),
+    dust = require('dustjs-helpers'),
+    pg = require('pg'),
+    app = express();
 
+    //connection string to database
+var connect = "postgres://pressstart_admin:12345@localhost/PressStart_db"
+
+
+//getting element from button
+let marketButton =document.getElementById('market');
+//event listener so when button is clicked to redirets user to specified website
 marketButton.addEventListener('click', () =>{
 window.open('https://www.pricecharting.com/category/video-games', '_blank')
 });
