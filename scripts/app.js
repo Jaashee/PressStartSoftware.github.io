49d58afe9fd10905063647b009966e98fb1eca58
Liamm602 <liam.barton@dcmail.ca>
2023-10-20 21:46:47 -0400

    added back java script for market button

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,10 @@
-/*let marketButton =document.getElementById('market');
+let marketButton =document.getElementById('market');
 let market = 'https://www.pricecharting.com/category/video-games';
 
 marketButton.addEventListener('click', () =>{
 window.open('https://www.pricecharting.com/category/video-games', '_blank')
-
+});
+/*
 var express = require('express'),
     path = require('path'),
     bodyParser = require('body-parser'),
