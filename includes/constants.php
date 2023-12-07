4917005e6192f3218f5ecaa2e32a88c6993cb15a
Liamm602 <liam.barton@dcmail.ca>
2023-12-07 07:44:37 -0500

    reset

--- includes/constants.php
+++ includes/constants.php
@@ -2,7 +2,7 @@
     define("DB_HOST",'127.0.0.1');
     define("DATABASE","PressStart_db");
     define("DB_ADMIN","pressstart_admin");
-    define("DB_PORT","5432");
+    define("DB_PORT","5433");
     define("DB_PASSWORD", "12345");
 
     ?>
--- inventory.php
+++ inventory.php
@@ -5,9 +5,10 @@ if (!isset($_SESSION['employee_id'])) {
     Header("Location: login.php");
 } ?>
     <body class="inventory-page">
-        <div class="main-content">
-            <div class="container">
-                <h1>Inventory Page</h1>
+    <div class="main-content">
+        <div class="container">
+            <div class="inventory-header">
+                <h1 class="inventory-title">Inventory Page</h1>
                 <div class="inventory-links">
                     <a href="gameinventory.php" class="inventory-link">Game Inventory</a>
                     <a href="consoleinventory.php" class="inventory-link">Console Inventory</a>
@@ -15,6 +16,7 @@ if (!isset($_SESSION['employee_id'])) {
                 </div>
             </div>
         </div>
+    </div>
     </body>
 
 <?php include './includes/footer.php';
