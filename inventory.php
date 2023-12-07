26b7e78f99932cf052242e23e1766d6fbb1e564b
Jaashee <jashakan2000@gmail.com>
2023-12-07 07:32:23 -0500

    Update to inv links to btns

--- inventory.php
+++ inventory.php
@@ -4,32 +4,17 @@
 if (!isset($_SESSION['employee_id'])) {
     Header("Location: login.php");
 } ?>
-    <div class="main-content">
-        <div class="container">
-            <h1>Inventory Page</h1>
-            <div>
-                <a href="gameinventory.php">
-                    <span class="nav-item">Game Inventory</span>
-                    <i class="fa-solid fa-arrow-right"></i>
-
-                </a>
-                <br>
-                <a href="consoleinventory.php">
-                    <span class="nav-item">Console Inventory</span>
-                    <i class="fa-solid fa-arrow-right"></i>
-
-                </a>
-                <br>
-                <a href="accessoryinventory.php">
-                    <span class="nav-item">Accessories inventory</span>
-                    <i class="fa-solid fa-arrow-right"></i>
-
-                </a>
-
+    <body class="inventory-page">
+        <div class="main-content">
+            <div class="container">
+                <h1>Inventory Page</h1>
+                <div class="inventory-links">
+                    <a href="gameinventory.php" class="inventory-link">Game Inventory</a>
+                    <a href="consoleinventory.php" class="inventory-link">Console Inventory</a>
+                    <a href="accessoryinventory.php" class="inventory-link">Accessories Inventory</a>
+                </div>
             </div>
-
         </div>
-    </div>
-
+    </body>
 
 <?php include './includes/footer.php';
